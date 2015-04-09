618b62edfd7bf2a796184ab1a0c0eeef7e61e0f3
Kristoffer Uggla Lingvall <totte.ul@gmail.com>
2015-04-09 19:46:03 +0200

    Implemented a basic search box

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>IR15: Image Search</title>
+		
+		<meta charset="utf-8" />
+		
+		<link rel="stylesheet" type="text/css" href="stylesheets/main.css" media="screen" />
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro">
+		
+		<script type="text/javascript" src="javascripts/main.js"></script>
+	</head>
+	
+	<body>
+		<h1>Image Search</h1>
+
+		<form action="" method="get">
+			<input type="text" name="query" autofocus="autofocus" />
+			<input type="submit" value="Search" />
+		</form>
+	</body>
+</html>
